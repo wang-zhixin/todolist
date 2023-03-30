0074ae7320dfc648add5b0cc697be3864b9bb683
王志鑫 <wangzhixin@pukanghealth.com>
2023-03-30 21:27:33 +0800

    add: init client side

---
+++ todolist-client/components/TodoItem.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import { RadioGroup } from '@headlessui/react'
+
+export function TodoList() {
+  let [plan, setPlan] = useState('startup')
+
+  return (
+    <RadioGroup
+      value={plan}
+      onChange={setPlan}
+    >
+      <RadioGroup.Label>Plan</RadioGroup.Label>
+      <RadioGroup.Option value='startup'>{({ checked }) => <span className={checked ? 'bg-blue-200' : ''}>Startup</span>}</RadioGroup.Option>
+      <RadioGroup.Option value='business'>{({ checked }) => <span className={checked ? 'bg-blue-200' : ''}>Business</span>}</RadioGroup.Option>
+      <RadioGroup.Option value='enterprise'>{({ checked }) => <span className={checked ? 'bg-blue-200' : ''}>Enterprise</span>}</RadioGroup.Option>
+    </RadioGroup>
+  )
+}
